25717d55ee3e27cd004e7abf22167fd6d7ea0420
Marcus <139352735+sucrammal@users.noreply.github.com>
2025-01-02 00:41:23 +0800

    Added lives component

---
+++ client/src/components/helpers/Lives.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+import '../../App.css'; 
+
+const Lives = ({ numLives }) => {
+    const heartImage = `${process.env.PUBLIC_URL}/assets/lives_heart.png`; // Public URL for the image
+    return (
+        <div className="lives-container">
+            <img src={heartImage} alt="Heart" className="heart-image" />
+            <span className="lives-count">{numLives}</span>
+        </div>
+    );
+};
+
+export default Lives;
